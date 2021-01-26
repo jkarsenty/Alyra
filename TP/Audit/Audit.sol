441c31a76770552efdbaf4f55009f96d37130dcc
jkarsenty <karsentyjk@gmail.com>
2021-01-26 16:53:39 +0100

    Add 1rst version of Audit

---
+++ TP/Audit/Audit.sol
@@ -0,0 +1,50 @@
+pragma solidity 0.6.11;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/math/SafeMath.sol";
+
+contract Crowdsale {
+    using SafeMath for uint256;
+
+    // TO DO :
+    // - créer des evenements car il n'y en a aucun
+
+    uint256 public savedBalance; // Total amount raised in ETH
+    address public owner; // the owner of the contract
+    address payable escrow; // wallet to collect raised ETH
+
+    mapping(address => uint256) public balances; // Balances in incoming Ether
+
+    // Initialization
+    constructor(address payable _escrow) public {
+        require(_escrow != address(0));
+        owner = msg.sender;
+        // add address of the specific contract
+        escrow = _escrow;
+    }
+
+    // function to receive ETH
+    receive() external payable {
+        require(msg.value != 0); // amount received is not null
+        balances[msg.sender] = balances[msg.sender].add(msg.value);
+        savedBalance = savedBalance.add(msg.value);
+
+        // TO DO :
+        // - do not use .send but use transfer instead or use .call{value: amount}("") (voir §3)
+        // - besoin de gerer l'exception si l'envoi echoue (voir §4 )
+        escrow.transfer(msg.value);
+    }
+
+    // refund investisor
+    function withdrawPayments() public {
+        require(balances[msg.sender] != 0);
+        address payable payee = msg.sender;
+        uint256 payment = balances[payee];
+
+        savedBalance = savedBalance.sub(payment);
+        balances[payee] = 0;
+
+        // TO DO :
+        // - do not use .send but use transfer instead or use .call{value: amount}("") (voir §3)
+        // - besoin de gerer l'exception si l'envoi echoue (voir §4 )
+        payee.transfer(payment);
+    }
+}
---
+++ TP/Audit/Audit_with_comments.sol
@@ -0,0 +1,61 @@
+// AUDIT : do not use the ^ but use directly a version of solidity
+pragma solidity ^0.5.12;
+
+// AUDIT : Miss the import
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/math/SafeMath.sol";
+
+contract Crowdsale {
+    using SafeMath for uint256;
+
+    // AUDIT : créer des evenements car il n'y en a aucun
+    // AUDIT : use pullPayment method
+    // AUDIT : Add some require
+
+    // AUDIT : better to use the audited librairy of Zeppelin with is Ownable
+    address public owner; // the owner of the contract
+
+    // AUDIT : escrow must be payable and not public
+    address public escrow; // wallet to collect raised ETH
+
+    // AUDIT : No need to initiate the variable with "= 0"
+    uint256 public savedBalance = 0; // Total amount raised in ETH
+
+    mapping(address => uint256) public balances; // Balances in incoming Ether
+
+    /* AUDIT : probleme name of function is name of contract
+    if it's initialization must be constructor ? */
+    // Initialization
+    function Crowdsale(address _escrow) public {
+        // AUDIT : Must not use tx.origin (voir §tx.origin)
+        owner = tx.origin;
+
+        // add address of the specific contract
+        escrow = _escrow;
+    }
+
+    // AUDIT : probleme name of function missing
+    // AUDIT : need to be external and payable if it's for receive ETH
+    // function to receive ETH
+    function() public {
+        balances[msg.sender] = balances[msg.sender].add(msg.value);
+        savedBalance = savedBalance.add(msg.value);
+
+        // AUDIT : do not use .send but use transfer instead or use .call{value: amount}("") (voir §3)
+        // AUDIT : besoin de gerer l'exception si l'envoi echoue (voir §4 )
+        escrow.send(msg.value);
+    }
+
+    // refund investisor
+    function withdrawPayments() public {
+        address payee = msg.sender;
+        uint256 payment = balances[payee];
+
+        // AUDIT : do not use .send but use transfer instead or use .call{value: amount}("") (voir §3)
+        // AUDIT : besoin de gerer l'exception si l'envoi echoue (voir §4 )
+        payee.send(payment);
+
+        // AUDIT : do not change a state after a .call() or contract.method()
+        savedBalance = savedBalance.sub(payment);
+        balances[payee] = 0;
+    }
+}
